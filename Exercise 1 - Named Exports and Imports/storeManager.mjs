faebc9a138d1e47ab033495da75a7e493dc71fca
Laruschkaj <laruschkajoubert@gmail.com>
2025-05-02 14:34:02 -0400

    Commit: Exercise 1

---
+++ Exercise 1 - Named Exports and Imports/storeManager.mjs
@@ -0,0 +1,10 @@
+import {addItem, removeItem, listItems} from "./inventory.mjs";
+
+/*to test functions:
+addItem("Book");
+addItem("Pen");
+addItem("Pencil");
+listItems();
+
+removeItem("Pen");
+listItems();*/
